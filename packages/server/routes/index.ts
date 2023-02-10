2bfcbb2c4b32fc578b2e79e3e30033e462c51970
Andrew <mkalpha@googlemail.com>
2023-02-10 08:09:53 +0000

    chore: Rename Zod import to be more explicit

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,16 +1,16 @@
 import { initTRPC, inferAsyncReturnType } from "@trpc/server";
-import { z } from "zod";
+import { z as zod } from "zod";
 import { Context } from "../index";
 
 export const trpc = initTRPC.context<Context>().create();
 
 const routes = {
-  getUser: trpc.procedure.input(z.string()).query((req) => {
+  getUser: trpc.procedure.input(zod.string()).query((req) => {
     req.input;
     return { id: req.input, name: "bilbo" };
   }),
   createUser: trpc.procedure
-    .input(z.object({ name: z.string().min(5) }))
+    .input(zod.object({ name: zod.string().min(5) }))
     .mutation(async (req) => {
       const user = { id: 1, name: "andrew" };
       return user;
